74221db2b7d679eaaec4db3e0f9c6afac224d021
Oliver Christensen <orc13a@live.dk>
2022-04-06 10:56:45 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+let game;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
     // 'Inter' er en skifttype fra Google Fonts som vi indlæser i index.html
     textFont('Inter');
     textSize(16);
+
+    game = new Asteriodgame();
 }
 
 function draw() {
