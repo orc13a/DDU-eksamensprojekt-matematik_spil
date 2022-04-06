2e9aa268df0be94e6dcdbb1132729553d7e30b8d
Oliver Christensen <orc13a@live.dk>
2022-04-06 09:38:27 +0200

    font

---
+++ app.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    // 'Inter' er en skifttype fra Google Fonts som vi indlæser i index.html
+    textFont('Inter');
+    textSize(16);
+}
+
+function draw() {
+    background(220);
+}
